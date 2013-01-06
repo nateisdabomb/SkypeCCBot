47484b1f7fcacf128258b4727d6817ec09c024a4
Richard Pupnewfster <pupnewfster@gmail.com>
2013-01-06 18:20:36 -0500

    added bot

---
+++ Form1.Designer.cs
@@ -0,0 +1,133 @@
+﻿using System;
+
+namespace CCBot
+{
+    partial class CCBot
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(CCBot));
+            this.txtMessage = new System.Windows.Forms.TextBox();
+            this.gBChat = new System.Windows.Forms.GroupBox();
+            this.txtLog = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.Contacts = new System.Windows.Forms.ListBox();
+            this.btnRefresh = new System.Windows.Forms.Button();
+            this.gBChat.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // txtMessage
+            // 
+            this.txtMessage.Location = new System.Drawing.Point(52, 409);
+            this.txtMessage.Name = "txtMessage";
+            this.txtMessage.Size = new System.Drawing.Size(399, 20);
+            this.txtMessage.TabIndex = 0;
+            this.txtMessage.Text = " ";
+            this.txtMessage.KeyDown += new System.Windows.Forms.KeyEventHandler(this.txtMessage_KeyDown);
+            // 
+            // gBChat
+            // 
+            this.gBChat.Controls.Add(this.txtLog);
+            this.gBChat.Font = new System.Drawing.Font("Calibri", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.gBChat.Location = new System.Drawing.Point(12, 12);
+            this.gBChat.Name = "gBChat";
+            this.gBChat.Size = new System.Drawing.Size(452, 391);
+            this.gBChat.TabIndex = 33;
+            this.gBChat.TabStop = false;
+            this.gBChat.Text = "Chat";
+            // 
+            // txtLog
+            // 
+            this.txtLog.BackColor = System.Drawing.SystemColors.Window;
+            this.txtLog.Location = new System.Drawing.Point(16, 20);
+            this.txtLog.Multiline = true;
+            this.txtLog.Name = "txtLog";
+            this.txtLog.ReadOnly = true;
+            this.txtLog.ScrollBars = System.Windows.Forms.ScrollBars.Both;
+            this.txtLog.Size = new System.Drawing.Size(423, 365);
+            this.txtLog.TabIndex = 0;
+            this.txtLog.Text = " ";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Calibri", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(9, 413);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(31, 13);
+            this.label1.TabIndex = 34;
+            this.label1.Text = "Chat:";
+            // 
+            // Contacts
+            // 
+            this.Contacts.FormattingEnabled = true;
+            this.Contacts.Location = new System.Drawing.Point(470, 22);
+            this.Contacts.Name = "Contacts";
+            this.Contacts.Size = new System.Drawing.Size(125, 381);
+            this.Contacts.TabIndex = 35;
+            // 
+            // btnRefresh
+            // 
+            this.btnRefresh.Location = new System.Drawing.Point(470, 414);
+            this.btnRefresh.Name = "btnRefresh";
+            this.btnRefresh.Size = new System.Drawing.Size(125, 24);
+            this.btnRefresh.TabIndex = 36;
+            this.btnRefresh.Text = " Refresh";
+            this.btnRefresh.UseVisualStyleBackColor = true;
+            this.btnRefresh.Click += new System.EventHandler(this.btnRefresh_Click);
+            // 
+            // CCBot
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(599, 441);
+            this.Controls.Add(this.btnRefresh);
+            this.Controls.Add(this.Contacts);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.gBChat);
+            this.Controls.Add(this.txtMessage);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "CCBot";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.gBChat.ResumeLayout(false);
+            this.gBChat.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtMessage;
+        private System.Windows.Forms.GroupBox gBChat;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtLog;
+        private System.Windows.Forms.ListBox Contacts;
+        private System.Windows.Forms.Button btnRefresh;
+    }
+}
---
+++ Form1.cs
@@ -0,0 +1,129 @@
+﻿using System;
+using System.Windows.Forms;
+using SKYPE4COMLib;
+using System.Collections.Generic;
+using System.IO; //Skype api
+
+namespace CCBot
+{
+    public partial class CCBot : Form
+    {
+        private Skype skype;
+        private const string trigger = "!"; // Say !help
+        private const string nick = "CCBot";
+        List<string> langList = new List<string>();
+
+        public CCBot()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            skype = new Skype();
+            skype.Attach(8, false);
+            // Listen 
+            skype.MessageStatus += new _ISkypeEvents_MessageStatusEventHandler(skype_MessageStatus);
+        }
+        private void skype_MessageStatus(ChatMessage msg, TChatMessageStatus status)
+        {
+            WriteLine(msg.Chat.Topic + ": " + msg.Sender.FullName.ToString() + ": " + msg.Body);
+            // Proceed only if the incoming message is a trigger
+            if (msg.Body.Length > 20 && msg.Body.Equals(msg.Body.ToUpper()))
+            {
+                msg.Chat.SendMessage(msg.Sender.FullName + ", why are you using caps?");
+                return;
+            }
+            langList = new List<string>(msg.Body.Split());
+            int count = 0;
+            if (!File.Exists("badwords.txt")) File.Create("badwords.txt");
+            foreach (string i in langList)
+            {
+                foreach (string line in File.ReadAllLines("badwords.txt"))
+                {
+                    if (i.ToLower().StartsWith(line))
+                    {
+                        count = count + 1;
+                    }
+                }
+            }
+            if (count > 0)
+            {
+                msg.Chat.SendMessage(msg.Sender.FullName + ", why are you using bad language?");
+                return;
+            }
+            if (msg.Body.IndexOf(trigger) == 0)
+            {
+                // Remove trigger string and make lower case
+                string command = msg.Body.Remove(0, trigger.Length).ToLower();
+                // Send processed message back to skype chat window
+                //skype.SendMessage(msg.Sender.Handle, nick + " Says: " + ProcessCommand(command));
+                msg.Chat.SendMessage(ProcessCommand(command));
+                WriteLine(ProcessCommand(command));
+            }
+        }
+        public void WriteLine(string s)
+        {
+            txtLog.AppendText(s);
+            txtLog.AppendText("\r\n");
+        }
+        private void txtMessage_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Enter)
+            {
+                string text = txtMessage.Text.Trim();
+                if (String.IsNullOrEmpty(text)) return;
+                WriteLine(text);
+                IChat ichat = skype.get_Chat(Contacts.GetItemText(Contacts.SelectedItem));
+                ichat.SendMessage("test");
+                txtMessage.Clear();
+            }
+        }
+        private string ProcessCommand(string str)
+        {
+            string result = "";
+            switch (str)
+            {
+                case "hello":
+                    result = "Hello!";
+                    break;
+                case "test":
+                    foreach (IChat chat in skype.Chats)
+                    {
+                        result += chat.FriendlyName + ", ";
+                    }
+                    break;
+                case "help":
+                    result = "Sorry no help available";
+                    break;
+                case "spam":
+                    result = "spam";//will cause spamm if haz a ! added
+                    break;
+                case "date":
+                    result = "Current Date is: " +
+                             DateTime.Now.ToLongDateString();
+                    break;
+                case "time":
+                    result = "Current Time is: " +
+                             DateTime.Now.ToLongTimeString();
+                    break;
+                default:
+                    result = "Sorry, I do not recognize your command";
+                    break;
+            }
+            return result;
+        }
+        private void contacts()
+        {
+            Contacts.Items.Clear();
+            foreach (IChat chat in skype.Chats)
+            {
+                Contacts.Items.Add(chat.Name);
+            }
+        }
+        private void btnRefresh_Click(object sender, EventArgs e)
+        {
+            contacts();
+        }
+    }
+}
